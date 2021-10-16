015074e9da595dc51ce24dbfba25e741ca655221
Simon Gonand <simongonand@gmail.com>
2021-10-16 11:30:25 +0200

    Camera is only following z axis

--- Assets/FollowingBoat.cs
+++ Assets/FollowingBoat.cs
@@ -16,6 +16,6 @@ public class FollowingBoat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = boat.position + initialOffset;
+        transform.position = new Vector3(0, 0,boat.position.z) + initialOffset;
     }
 }
