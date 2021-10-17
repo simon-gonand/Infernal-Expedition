a40eedc5cbce507279d635a57f260a52703b5c3e
Simon Gonand <simongonand@gmail.com>
2021-10-17 18:29:07 +0200

    Add four players + link them to the boat
    Players move with the boat by itself

--- Assets/PlayerAttach.cs
+++ Assets/PlayerAttach.cs
@@ -4,27 +4,21 @@ using UnityEngine;
 
 public class PlayerAttach : MonoBehaviour
 {
-    private GameObject container;
     private void OnTriggerEnter(Collider other)
     {
-        // prevent player object to be deformed
-        // https://stackoverflow.com/questions/63351469/object-deforms-when-it-becomes-a-child-of-a-gameobject
         if (other.tag == "Player")
         {
-            container = new GameObject("container");
-
-            Vector3 myScale = transform.localScale;
-            container.transform.localScale = new Vector3(1f/myScale.x, 1f/myScale.y, 1f/myScale.z);
-
-            container.transform.SetParent(transform, false);
-            other.gameObject.transform.SetParent(container.transform);
+            other.gameObject.transform.SetParent(transform);
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
         }
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Player")
+        {
             other.gameObject.GetComponent<Rigidbody>().velocity = this.GetComponent<Rigidbody>().velocity;
+        }
     }
 
     private void OnTriggerExit(Collider other)
@@ -32,7 +26,7 @@ public class PlayerAttach : MonoBehaviour
         if (other.tag == "Player")
         {
             other.gameObject.transform.SetParent(null);
-            Destroy(container);
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
         }
     }
 }
