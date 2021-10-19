c87e6576d916f9eb8a921ebce452d0ca6b760b2a
Simon <simongonand@gmail.com>
2021-10-19 13:44:32 +0200

    Player movement fixed!

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,23 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
+    [Header("Stat")]
     private float playerSpeed = 2.0f;
-    [SerializeField]
-    private CharacterController controller;
+
+    [Header("Boat References")]
     [SerializeField]
     private Rigidbody boatRigidbody;
     [SerializeField]
     private GameObject boatObj;
     [SerializeField]
+    private Transform boatTransform;
+
+    [Header("Self References")]
+    [SerializeField]
     private Transform self;
+    [SerializeField]
+    private Rigidbody selfRigidbody;
 
     private Vector2 playerMovementInput = Vector2.zero;
     private bool isOnBoat;
-    private Vector3 currentMove;
+    private GameObject container;
 
     // Start is called before the first frame update
     void Start()
@@ -36,20 +42,31 @@ public class PlayerController : MonoBehaviour
     {
         if (other.gameObject == boatObj)
             isOnBoat = true;
+
+        container = new GameObject("container");
+
+        Vector3 myScale = boatTransform.localScale;
+        container.transform.localScale = new Vector3(1f/myScale.x, 1f/myScale.y, 1f/myScale.z);
+
+        container.transform.SetParent(boatTransform, false);
+        self.SetParent(container.transform);
+        selfRigidbody.isKinematic = true;
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.gameObject == boatObj)
             isOnBoat = false;
+
+        self.SetParent(null);
+        Destroy(container);
+        selfRigidbody.isKinematic = false;
     }
 
     private void PlayerMovement()
     {
         Vector3 move = new Vector3(playerMovementInput.x, 0.0f, playerMovementInput.y);
-        if (isOnBoat && boatRigidbody.velocity != Vector3.zero)
-            move.Scale(boatRigidbody.velocity);
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        self.Translate(move * Time.deltaTime * playerSpeed, Space.World);
         if (move != Vector3.zero)
             self.forward = move;
     }
