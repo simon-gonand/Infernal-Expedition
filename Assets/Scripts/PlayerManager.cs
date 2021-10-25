3dca47c5dc584a635e3b48c74059ecd1d623dc1f
Simon <simongonand@gmail.com>
2021-10-25 10:19:46 +0200

    Players are not spawning on each other

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -17,6 +17,7 @@ public class PlayerManager : MonoBehaviour
 
     public void OnPlayerJoined(PlayerInput playerInput)
     {
+        float playerSpawnOffset = 0.0f;
         switch (playerInput.playerIndex)
         {
             case 0:
@@ -24,18 +25,22 @@ public class PlayerManager : MonoBehaviour
                 break;
             case 1:
                 playerInput.gameObject.GetComponent<MeshRenderer>().material = player2Material;
+                playerSpawnOffset = 0.5f;
                 break;
             case 2:
                 playerInput.gameObject.GetComponent<MeshRenderer>().material = player3Material;
+                playerSpawnOffset = -0.5f;
                 break;
             case 3:
                 playerInput.gameObject.GetComponent<MeshRenderer>().material = player4Material;
+                playerSpawnOffset = 1.0f;
                 break;
             default:
                 break;
         }
         Vector3 playerSpawnPosition = BoatMovements.instance.transform.position;
         playerSpawnPosition.y += 1.0f;
+        playerSpawnPosition.z += playerInput.playerIndex * playerSpawnOffset;
         playerInput.gameObject.transform.position = playerSpawnPosition;
     }
 }
